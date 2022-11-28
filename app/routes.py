90ab6d8513e152bbcb6b3bff09c6cbe7ef2eda12
Eden <eden.candelas@gmail.com>
2022-11-28 13:46:11 -0600

    Parte 2 Templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello World!"
+  user = {'username': 'EdenLeonardo'}
+  posts = [
+    {
+      'author': {'username': 'John'},
+      'body': "Beautiful day to devour ribs!"
+    },
+    {
+      'author': {'username': 'Mady'},
+      'body': "The Wakandians movie was soooo good!"
+    }
+  ]
+  return render_template('index.html', title="home", user=user, posts=posts)
